9ecfd55934f1ca90849f0e9f2fafeb06b69404df
André Cruz <andremiguelcruz@msn.com>
2020-02-02 10:38:19 +0000

    fix: add missing require

--- src/LayoutManager.js
+++ src/LayoutManager.js
@@ -9,7 +9,7 @@ export default class LayoutManager extends Component {
         Component: PropTypes.elementType.isRequired,
         pageProps: PropTypes.object,
         defaultLayout: PropTypes.element,
-        children: PropTypes.func,
+        children: PropTypes.func.isRequired,
     };
 
     static defaultProps = {
